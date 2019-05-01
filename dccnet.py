c190cb0f2a90ad1e5deb293571794200624808bf
scarletgv <33155764+scarletgv@users.noreply.github.com>
2019-05-01 18:29:57 -0300

    Update dccnet.py

--- dccnet.py
+++ dccnet.py
@@ -1,10 +1,16 @@
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""
-Created on Fri Apr 19 18:38:43 2019
+'''
+UFMG - ICEx - DCC - Redes de Computadores - 2019/1
 
-@author: Scarlet
-"""
+TP1: DCCNET -- Camada de Enlace
 
+Aluna: Scarlet Gianasi Viana
+Matrícula: 2016006891
+
+Versão utilizada: Python 3.6.7
+
+'''
 import sys
 import socket
 
@@ -33,7 +39,7 @@ def leDados(entrada):
                 break
         else:
             dados.append(ord(segmento))
-            tamanho += 1            
+            tamanho += 1  
     return tamanho, dados
 
 def constroiQuadroDados(entrada, ID):    
@@ -67,134 +73,80 @@ def encode16 (hexList):
     return frame
 
 def decode16(quadro):   
-    hexList = [eval('0x'+quadro[i:i+2]) for i in range(0, len(quadro), 2)]    
-    return hexList
+    quadroDecoded = [eval('0x'+quadro[i:i+2]) for i in range(0, len(quadro), 2)]    
+    return quadroDecoded
 
-def unstuff (frame):
+def unstuff (quadro):
     unstuffingList = []
     i = 5
     shift = 0
-    while i < len(frame)-1:
-        if frame[i] == DLE:
+    while i < len(quadro)-1:
+        if quadro[i] == DLE:
             unstuffingList.append(i)
             i += 1
         i += 1
     for item in unstuffingList:
-        frame.pop(item-shift)
+        quadro.pop(item-shift)
         shift += 1
 
 def checksum(quadro):
     tamanho = len(quadro)
     posicao = 0
-    soma = 0   
+    soma = 0      
     while tamanho > 1:
         bh = quadro[posicao]
         bl = quadro[posicao+1]
-        posicao = posicao + 2
-        tamanho = tamanho - 2
-        soma = soma + (bh << 8) + bl    
+        posicao += 2
+        tamanho -= 2
+        soma += (bh << 8) + bl         
     if tamanho > 0:
-        soma = soma + quadro[posicao]   
+        soma += quadro[posicao]         
     while (soma >> 16):
-        soma = (soma & 0xffff) + (soma >> 16)   
+        soma = (soma & 0xffff) + (soma >> 16)       
     soma = 0xffff - soma    
-    byte1 = soma >> 8
-    byte2 = soma & 0xff   
+    byte2 = soma >> 8
+    byte1 = soma & 0xff  
     return byte1, byte2
 
 def checksumValido(quadroInteiro):
-    
     quadroDcd = decode16(quadroInteiro)
-    quadroDcd[3] = 0
-    quadroDcd[4] = 0
-    b1, b2 = checksum(quadroDcd)
-                        
-    if (b1 == quadroDcd[3]) & (b2 == quadroDcd[4]):
+    byte1, byte2 = checksum(quadroDcd)
+    if (byte1 == 0) & (byte2 == 0):
         return True
     else:
         return False
     
-def escreveDados(saida, quadro):    
-    quadroDcd = decode16(quadro) 
+def escreveDados(arquivoS, quadroDcd):    
     tam = len(quadroDcd)
     dados = quadroDcd[5:(tam-1)] 
-    dadosB = [dados[i].to_bytes(1, byteorder='big') for i in range(0, len(dados))]
-
-    for byte in dadosB:
-        saida.write(byte)
-    
+    dadosB = [dados[i].to_bytes(1, byteorder='big') for i in range(0, len(dados))]  
+    with open(arquivoS, "ab") as saida: 
+        for byte in dadosB:
+            saida.write(byte)  
     print("Fim da escrita do quadro na saida.")
-    
 
-def trocaMensagens(s, arquivoE, arquivoS):
-    IDe = 0
-    IDr = 0
-    entrada = open(arquivoE, "rb")
-    saida = open(arquivoS, "wb")
-    ack = False
+def enviaQuadro(s, quadro):
+    quadroEnc = encode16(quadro)
+    print(quadroEnc)
+    s.send(quadroEnc.encode('ascii'))
     
-    s.settimeout(2)
-
-    quadro = constroiQuadroDados(entrada, IDe)
-    quadro16 = encode16(quadro)
-    s.send(quadro16.encode('ascii')) # Envia o primeiro quadro
-    
-    while True:
-        while not ack: # Enquanto não receber ack do quadro enviado:
-            try:
-                byte = s.recv(2) # Recebe um byte
-                byteH = byte.decode('ascii')
-                if byteH == 'cc': # SOF, inicio de quadro
-                    quadro, flags = leQuadro(s, IDr, IDe)
-                    if quadro == '': # Quadro descartado
-                        print("Quadro descartado")
-                        break
-                    # Se recebeu um quadro de dados, escrever
-                    # no arquivo de saida
-                    else:
-                        # Recebeu um quadro valido
-                        if flags == '7f': # Quadro de dados
-                            print("Quadro de dados valido recebido. ")
-                            
-                            ##### SOMENTE DAZER ISSO DEPOIS
-                            ##### DE UNSTUFFING!!!!!!!!!!!!!!!
-                            
-                            escreveDados(saida, quadro)
-                            # Escreve no arquivo de saida
-                        else: # Quadro ack
-                            print("Quadro ack valido recebido.")
-                            #### CRIAR OUTRO QUADRO
-                            ### ENVIAR QUADRO
-                            ack = True # Sai do loop e entra de novo
-                        # Envia mais um quadro
-                else:
-                    print("") # Continua recebendo bytes ate receber um inicio de quadro
-                    
-            except socket.timeout as t:
-                print(t)    
-                #retransmite quadro
-                s.send(quadro16.encode('ascii'))
-      
-        #transmite proximo quadro
-    
-def leQuadro(s, IDr, IDe):
-    quadroInteiro = 'cc'
+def leQuadro(s, IDenviado, IDreceptor):
+    quadroInteiro = 'cc' # SOF
     flags = '' # Inicializacao
     msg = s.recv(2)
     ID = msg.decode('ascii')
+    print("ID:"+str(ID))
     quadroInteiro = quadroInteiro + ID
+    checksumAnterior = ''
     #recebe o resto do quadro até EOF
     while True:
         msg = s.recv(2)
         flags = msg.decode('ascii')
-        quadroInteiro += flags
-        
+        quadroInteiro += flags       
         if flags == '7f': # QUADRO DE DADOS
-            # continua lendo dados 
-            # TESTA ID
-            if ID == IDr: # Se for o ID esperado, continua           
+            if int(ID) == IDenviado: #receptor: # Se for o ID esperado, continua           
                 msg = s.recv(4)
-                checksum = msg.decode('ascii')
+                checksum = msg.decode('ascii')                
                 quadroInteiro += checksum                
                 i = 5     
                 while True:
@@ -206,113 +158,142 @@ def leQuadro(s, IDr, IDe):
                         msg = s.recv(2)
                         dadoEsc = msg.decode('ascii')
                         quadroInteiro += dadoEsc # Le o outro dado ignorando proximo
-                    elif dado == 'cd': # EOF CUIDADO COM OS DLE
-                        print("Final do quadro de dados encontrado.")
+                    elif dado == 'cd': # Se EOF 
+                        print("Final do quadro de dados encontrado.")                   
                         if checksumValido(quadroInteiro):
                             print("Quadro de dados com checksum valido!")
+                            checksumAnterior = checksum
                             return quadroInteiro, flags
                         else:
                             print("Checksum invalido! Quadro de dados descartado")
-                            return '', flags
-                    elif i > 512:
+                            return [], flags
+                    elif i > 518:
                         print("EOF não encontrado, quadro de dados descartado.")
-                        return '', flags
+                        return [], flags
                     i += 1               
             else:
-                print("Quadro de dados descartado, recebido com ID errado.")
-                return ''  , flags             
-        elif flags == '80': # QUADRO ACK          
-            if ID == IDe: # Se o ID do quadro for o ID de dados esperado continua
+                # DETECTAR RETRANSMISSAO
+                msg = s.recv(4)
+                checksum = msg.decode('ascii')
+                if checksum == checksumAnterior:
+                    print("Retransmissão detectada. Enviando outro quadro ack.")
+                    quadroAck = constroiQuadroAck(int(ID))
+                    enviaQuadro(s, quadroAck)
+                    return [], flags                  
+                else:                
+                    print("Quadro de dados descartado, recebido com ID errado.")
+                    return [], flags             
+        elif flags == '80': # ACK        
+            if int(ID) == IDreceptor: #enviado: 
                 msg = s.recv(4)
                 checksum = msg.decode('ascii')                
                 msg = s.recv(2)
                 byte = msg.decode('ascii')                
                 if byte == 'cd':
                     quadroInteiro += checksum + byte
-                    print("Final do quadro ACK encontrado.")
+                    print("Final do quadro ACK encontrado.")                   
                     if checksumValido(quadroInteiro):
                         print("Quadro ack com checksum valido!")
                         return quadroInteiro, flags
                     else:
                         print("Checksum invalido! Quadro ack descartado")
-                        return '', flags                   
+                        return [], flags                   
                 else:
                     print("Erro no quadro ACK.")
-                    return '', flags
-            else:
-                print("Quadro ACk descartado, recebido com ID errado")
-                return '', flags               
+                    return [], flags
+            else:                
+                print("Quadro Ack descartado, recebido com ID errado")
+                print(IDenviado)
+                return [], flags               
     print("ERRO")
-    return '', flags
+    return [], flags
 
-def leBytes1(arquivoE, n):
-    with open(arquivoE, "rb") as entrada:
-        data = entrada.read(n)
+def iniciaTroca(s, arquivoE, arquivoS):   
+    IDenviado = 0
+    IDreceptor = 0
+    entrada = open(arquivoE, "rb")
+    if not entrada:
+        print("Arquivo de entrada invalido.")
+        return
+    primeiroQuadro = constroiQuadroDados(entrada, IDenviado)   
+    if primeiroQuadro == []:
+        print("Arquivo de entrada vazio. Nao ha quadros para enviar.")
+    else:
+        print("Enviando quadro.")
+        enviaQuadro(s, primeiroQuadro)     
+        trocaMensagens(s, primeiroQuadro, entrada, arquivoS, IDenviado, IDreceptor)
     
-    entrada.close()
-    return data
-
-def escreveBytes1(arquivoS, data, n):
-    with open(arquivoS, "wb") as saida:
-        saida.write(data)
-    
-    saida.close()
-
-def troca(c, arquivoE, arquivoS):
-    envia(c, arquivoE)
-    recebe(c, arquivoS)
-
-def envia(c, arquivoE): 
-    frame = leBytes1(arquivoE, 10)
-    c.send(frame)
-
-def recebe(c, arquivoS):
-    msg = c.recv(2)
-    escreveBytes1(arquivoS, msg, 10)
-
-def main(): 
-    if sys.argv[1] == "-s":
+def trocaMensagens(s, primeiroQuadro, entrada, arquivoS, IDenviado, IDreceptor):   
+    quadroAtual = primeiroQuadro   
+    while True:
+        byte = s.recv(2)
+        byteHex = byte.decode('ascii')        
+        if byteHex == 'cc': # Inicio de quadro encontrado
+            print("Inicio de quadro.")
+            quadroLido, flags = leQuadro(s, IDenviado, IDreceptor)            
+            if quadroLido != []:
+                #print("Quadro invalido. Descartado.")
+            #else: # QUadro valido
+                if flags == '7f':  # DADOS
+                    print("Quadro de dados recebido com sucesso.")
+                    quadroDecoded = decode16(quadroLido)
+                    unstuff(quadroDecoded)
+                    escreveDados(arquivoS, quadroDecoded)
+                    
+                    print("Enviando quadro ACK.")
+                    quadroAck = constroiQuadroAck(IDenviado)
+                    enviaQuadro(s, quadroAck)   
+                    IDenviado = 1 - IDenviado # Troca o ID de recebimento
+                else:  
+                    print("Quadro ack recebido com sucesso.")
+                    IDreceptor = 1 - IDreceptor # Troca ID de envio
+                    novoQuadro = constroiQuadroDados(entrada, IDreceptor)                    
+                    if novoQuadro == []:
+                        print("Arquivo de entrada vazio. Nao ha quadros para enviar.")
+                    else:
+                        print("Enviando quadro.")                        
+                        quadroAtual = novoQuadro
+                        enviaQuadro(s, quadroAtual)
+                
+def main():    
+    if len(sys.argv) < 4:
+        print("Erro: número de argumentos insuficiente.")
+        return   
+    entrada = sys.argv[3]
+    saida = sys.argv[4]   
+    if entrada == saida:
+        print("Erro: não é possível utilizar o mesmo arquivo como entrada e saída.")
+        return    
+    elif sys.argv[1] == "-s":
         porta = int(sys.argv[2])
         endereco = ("", porta)
         print("Servidor: porta "+str(porta))
-        
-        ss = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        
+        ss = socket.socket(socket.AF_INET, socket.SOCK_STREAM)    
         endereco = ("", porta)
         ss.bind(endereco)
-        ss.listen(1)
-        
-        print("Esperando um cliente...")
-
+        ss.listen(1)       
+        print("Esperando o cliente...")
         s, cliente = ss.accept()
-        print("Conectado a {}".format(cliente))
-        
-        entrada = sys.argv[3]
-        saida = sys.argv[4]
-        
-        troca(s, entrada, saida)
-        
-        s.close()
-        ss.close()
-        
+        s.settimeout(None)
+        print("Conectado a {}".format(cliente))  
     elif sys.argv[1] == "-c":
         IP, porta = sys.argv[2].split(':')
-        print("Cliente: end IP: "+IP+" e porta: "+porta)
-        
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        
+        print("Cliente: end IP: "+IP+" e porta: "+porta)      
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)        
         endServidor = (IP, int(porta))
         s.connect(endServidor)
-        
-        entrada = sys.argv[3]
-        saida = sys.argv[4]
-        
-        troca(s, entrada, saida)
-        
-        s.close()   
+        s.settimeout(None)             
     else:
-        print("Comando invalido. Utilize '-s <PORT> <INPUT> <OUTPUT>' ou '-c <IP>:<PORT> <INPUT> <OUTPUT>'")
-        exit(1)
+        print("Comando invalido.")
+        return      
+    try:
+        iniciaTroca(s, entrada, saida)
+    except (KeyboardInterrupt, socket.error):
+        print("Finalizando o programa...")
+        s.shutdown(socket.SHUT_RDWR)
+        s.close()
+        if sys.argv[1] == "-s":
+            ss.close()    
 
 if __name__ == "__main__":
     main()
